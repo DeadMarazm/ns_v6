e2641c7aeb1056d567a78cc8d141fa96386a8433
Shamolet <crossfit2203@gmail.com>
2024-11-15 15:33:37 +0300

    init

--- app/forms/forms.py
+++ app/forms/forms.py
@@ -1,17 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class RegistrationForm(FlaskForm):
+    username = StringField('Никнейм', validators=[DataRequired()])
     email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired(), Length(min=8)])
     confirm_password = PasswordField('confirm_password', validators=[DataRequired(), EqualTo('password')])
     active = BooleanField('active', default=True)
+    submit = SubmitField('Зарегистрироваться')
+
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
+
 
 class LoginForm(FlaskForm):
     email = StringField('email', validators=[DataRequired(), Email()])
     password = PasswordField('password', validators=[DataRequired()])
     remember = BooleanField('remember')
+    submit = SubmitField('Тык')
--- app/routes/admin/views.py
+++ app/routes/admin/views.py
@@ -1,31 +1,48 @@
+from flask import redirect, url_for, request, render_template
 from flask_admin import AdminIndexView, expose
 from flask_admin.contrib.sqla import ModelView
-from flask_security import roles_required
+from flask_admin.menu import MenuLink
+from flask_security import current_user
 from app.extensions import admin, db
-from app.forms.forms import RegistrationForm
 from app.models.models import User, Role
 
 
+class SecureModelView(ModelView):
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.has_role('admin')
+
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('admin_bp.admin_login', next=request.url))
+
+
 class MyAdminIndexView(AdminIndexView):
-    @expose('/')
-    @roles_required('admin')  # Ограничение доступа только для пользователей с ролью 'admin'
+    @expose('/')  # Удаляем проверку прав доступа, она теперь в SecureModelView
     def index(self):
-        form = RegistrationForm()  # Create an instance of the form
-        return self.render('admin/index.html', form=form)  # Pass the form to the template
+        # Используем стандартное поведение Flask-Admin
+        return super(MyAdminIndexView, self).index()
 
-
-class UserView(ModelView):
+class UserAdmin(SecureModelView):
     column_list = ('id', 'email', 'active')
     can_edit = True
+    can_create = True
     can_delete = False
+    can_view_details = True
+    column_exclude_list = ['password']
+    form_excluded_columns = ['password']
+
+    form_columns = ['username', 'email', 'active']
 
 
-class RoleView(ModelView):
+class RoleAdmin(SecureModelView):
     column_list = ('id', 'name', 'description')
     can_edit = True
     can_delete = False
 
 
-admin.add_view(MyAdminIndexView(name='Admin', endpoint='admin'))
-admin.add_view(UserView(User, db.session, 'Users', endpoint='users'))
-admin.add_view(RoleView(Role, db.session, 'Roles', endpoint='roles'))
+def init_admin(app):
+    admin.init_app(app, index_view=MyAdminIndexView())  # Используем пользовательский класс MyAdminIndexView
+    admin.add_view(UserAdmin(User, db.session, name='Users', endpoint='admin_user'))
+    admin.add_view(RoleAdmin(Role, db.session, name='Roles', endpoint='admin_role'))
+
+    # Добавляем ссылку на выход
+    admin.add_link(MenuLink(name='Выход', endpoint='admin_bp.admin_logout'))
